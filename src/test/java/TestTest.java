f224b7261dec2ac090a245a5ddac1bca6c2f8770
MRFF <344362143@qq.com>
2021-11-29 14:51:54 +0800

    socket编程例题3-》阻塞问题处理（Branch）

--- src/test/java/TestTest.java
+++ src/test/java/TestTest.java
@@ -16,27 +16,40 @@ public class TestTest {
         System.out.println("客户端启动");
         Socket socket = null;
         OutputStream outputStream = null;
+        InputStream inputStream = null;
         FileInputStream fileInputStream = null;
         ByteArrayOutputStream byteArrayOutputStream = null;
         try {
             InetAddress inetAddress = InetAddress.getByName("127.0.0.1");
             socket = new Socket(inetAddress,9987);
             outputStream = socket.getOutputStream();
+            inputStream = socket.getInputStream();
+            byteArrayOutputStream = new ByteArrayOutputStream();
 
-            //读文件
+            //读文件并写到服务端
             fileInputStream = new FileInputStream("client.txt");
             int length = -1;
             byte[] temp = new byte[5];
-            byteArrayOutputStream = new ByteArrayOutputStream();
             while((length=fileInputStream.read(temp))!=-1)
             {
-                byteArrayOutputStream.write(temp,0,length);
+                outputStream.write(temp,0,length);
             }
-            byteArrayOutputStream.flush();//刷新
-
-            //写到服务端
-            outputStream.write(byteArrayOutputStream.toByteArray());
             outputStream.flush();
+            socket.shutdownOutput();
+
+            int length1 = -1;
+            byte[] temp1 = new byte[5];
+            while((length1=inputStream.read(temp1))!=-1)
+            {
+                byteArrayOutputStream.write(temp1,0,length1);
+            }
+            byteArrayOutputStream.flush();
+            socket.shutdownInput();
+
+            System.out.println(byteArrayOutputStream.toString());
+
+
+
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
@@ -49,11 +62,10 @@ public class TestTest {
                 }
 
             }
-            if(byteArrayOutputStream!=null)
+            if(inputStream!=null)
             {
-
                 try {
-                    byteArrayOutputStream.close();
+                    inputStream.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -76,6 +88,14 @@ public class TestTest {
                 }
 
             }
+            if(byteArrayOutputStream!=null)
+            {
+                try {
+                    byteArrayOutputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
 
     }
@@ -87,27 +107,32 @@ public class TestTest {
         ServerSocket serverSocket = null;
         Socket socket = null;
         InputStream inputStream = null;
-        ByteArrayOutputStream byteArrayOutputStream = null;
         FileOutputStream fileOutputStream = null;
+        OutputStream outputStream = null;
         try {
             serverSocket = new ServerSocket(9987);
             socket = serverSocket.accept();
             inputStream = socket.getInputStream();
+            outputStream = socket.getOutputStream();
+            fileOutputStream = new FileOutputStream("server.txt");
 
-            //开始读客户端发来的数据
+            //开始读客户端发来的数据并写到本地文件
             byte[] temp = new byte[5];
             int length = -1;
-            byteArrayOutputStream = new ByteArrayOutputStream();
             while((length = inputStream.read(temp))!=-1)
             {
-                byteArrayOutputStream.write(temp,0,length);
+                fileOutputStream.write(temp,0,length);
             }
-            byteArrayOutputStream.flush();
-
-            //开始写入本地文件
-            fileOutputStream = new FileOutputStream("server.txt");
-            fileOutputStream.write(byteArrayOutputStream.toByteArray());
             fileOutputStream.flush();
+            socket.shutdownInput();
+
+            System.out.println("服务端写入文件完毕");
+
+            outputStream.write("服务端写入文件完毕，向客户端发送通知".getBytes());
+            outputStream.flush();
+            socket.shutdownOutput();
+
+
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
@@ -120,20 +145,19 @@ public class TestTest {
                     e.printStackTrace();
                 }
             }
-            if(byteArrayOutputStream!=null)
-            {
-
-                try {
-                    byteArrayOutputStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
             if(inputStream!=null)
             {
 
                 try {
                     inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(outputStream!=null)
+            {
+                try {
+                    outputStream.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
